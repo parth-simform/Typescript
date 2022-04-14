fb771991ef088e1f5d15cb9210fcd11408a6ce84
Parth <parthkumar@simformsolutions.com>
2022-04-14 13:00:16 +0530

    deleted data

--- App.tsx
+++ App.tsx
@@ -1,18 +1,12 @@
+import {Text, SafeAreaView} from 'react-native';
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
 
-import DashBoard from './app/module/Dashboard';
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <DashBoard />
+    <SafeAreaView>
+      <Text>App</Text>
     </SafeAreaView>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e8e7e3',
-  },
-});
+
 export default App;
