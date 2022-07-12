405df153dafe3f31ce73c99e2f24c6f83715afc2
Parth <parthkumar@simformsolutions.com>
2022-07-12 11:46:34 +0530

    work on lookup

--- app/type.ts
+++ app/type.ts
@@ -3,19 +3,43 @@ export interface AllData {
   state?: string;
 }
 
-export interface childA extends AllData {
-  variant: 'A';
-  message: string;
-}
+// Lookup types
+//https://www.youtube.com/watch?v=eJ6R1knfsoc
 
-export interface childB extends AllData {
-  variant: 'B';
-  name: string;
-  state?: never;
-}
+type Child = {
+  childA: {
+    variant: 'A';
+    message: string;
+  };
+  childB: {
+    variant: 'B';
+    name: string;
+    state?: never;
+  };
+  childC: {
+    variant: 'C';
+    age: number;
+    mobile_number?: number;
+  };
+};
 
-export interface childC extends AllData {
-  variant: 'C';
-  age: number;
-  mobile_number?: number;
-}
+export type childA = Child['childA'] & AllData;
+export type childB = Child['childB'] & AllData;
+export type childC = Child['childC'] & AllData;
+
+// export interface childA extends AllData {
+//   variant: 'A';
+//   message: string;
+// }
+
+// export interface childB extends AllData {
+//   variant: 'B';
+//   name: string;
+//   state?: never;
+// }
+
+// export interface childC extends AllData {
+//   variant: 'C';
+//   age: number;
+//   mobile_number?: number;
+// }
